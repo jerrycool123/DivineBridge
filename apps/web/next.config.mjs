5b3b521a26b0e4c91e5e5d71d5cef205747e304c
jerrycool123 <jerrycool71534@gmail.com>
2024-02-13 07:38:27 +0800

    fix: webpack config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@divine-bridge/common'],
+  webpack: (config) => {
+    Object.assign(config.resolve.alias, {
+      '@mongodb-js/zstd': false,
+      '@aws-sdk/credential-providers': false,
+      'snappy': false,
+      'aws4': false,
+      'mongodb-client-encryption': false,
+      'kerberos': false,
+      'supports-color': false,
+    });
+    return config;
+  },
   images: {
     domains: ['cdn.discordapp.com', 'yt3.ggpht.com'],
   },
