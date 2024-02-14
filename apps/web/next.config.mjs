12b46bc051a4e55cef0d9899af8a6888393ea073
jerrycool123 <jerrycool71534@gmail.com>
2024-02-14 18:14:25 +0800

    chore: use nginx

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -25,14 +25,6 @@ const nextConfig = {
       },
     ],
   },
-  async rewrites() {
-    return [
-      {
-        source: '/server/:path*',
-        destination: `${process.env.SERVER_URL}/server/:path*`,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
