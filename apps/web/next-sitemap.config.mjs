9f29d2a922c365c96458c8de44a121e39edce2b9
jerrycool123 <jerrycool71534@gmail.com>
2025-01-19 19:27:33 +0800

    fix: missing siteUrl

--- apps/web/next-sitemap.config.mjs
+++ apps/web/next-sitemap.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const sitemapConfig = {
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
-  siteUrl: process.env.AUTH_URL ?? '',
+  siteUrl: process.env.NEXT_PUBLIC_WEB_URL ?? '',
   generateRobotsTxt: true,
   exclude: ['/dashboard*'],
 };
